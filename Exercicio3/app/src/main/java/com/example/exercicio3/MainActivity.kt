a8f1b078f8c13f961b6c37367d3ea7e65cb44211
gabrielavmx <villegasmonterogabriela76@gmail.com>
2024-04-02 21:53:51 -0300

    exercicios básicos

---
+++ Exercicio3/app/src/main/java/com/example/exercicio3/MainActivity.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Tuesday")
+    println("Thursday")
+    println("Wednesday")
+    println("Friday")
+    println("Monday")
+
+    //em ordem
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+    }
